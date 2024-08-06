648eaefca37aa636f4a75421ba4d12ea42e90298
wmezadev <5531063+wmezadev@users.noreply.github.com>
2024-08-06 16:37:45 -0500

    feat: adds card image sample and props

--- components/card/index.js
+++ components/card/index.js
@@ -1,47 +1,35 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
-const Card = () => {
+const Card = ({ name, image }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <>
-      <div className="p-6 rounded-xl border border-gray-200 hover:shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] transition-all duration-500">
-        <img src="/next.svg"  alt="" priority/>
-
-        <h2 className="text-xl font-semibold">
-          Lenovo Ideapad D330 10GL intel CDC N4020..
-        </h2>
-
-        <ul className="list-disc my-5 ps-5">
-          <li className="text-base font-medium text-gray-500">RAM-8GB</li>
-          <li className="text-base font-medium text-gray-500">
-            Processor Type - intel 7i
-          </li>
-          <li className="text-base font-medium text-gray-500">
-            Graphics memary - shared
-          </li>
-          <li className="text-base font-medium text-gray-500">
-            Display size (inch) - 15
-          </li>
-        </ul>
-
-        <div className="flex flex-wrap sm:flex-nowrap items-center justify-between gap-4">
-          <h3 className="grow w-1/2 text-xl font-semibold">$23000</h3>
-          <span className="inline-block shrink">
-            <a
-              href="#"
-              className="h-10 w-10 inline-flex items-center justify-center bg-gray-100 rounded-full"
-            >
-              <i className="ti ti-heart text-xl"></i>
-            </a>
-          </span>
-          <a
-            href="#"
-            className="grow w-1/2 py-2.5 flex items-center justify-center rounded-full text-white bg-blue-500 hover:bg-blue-600 transition-all duration-500 cursor-pointer"
-          >
-            Reservar
-          </a>
-        </div>
+    <div className="bg-white p-6 rounded-xl border border-gray-200 hover:shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] transition-all duration-500">
+      <div className="flex justify-center ml-auto mr-auto max-w-md">
+        <Image
+          src={image}
+          alt={name}
+          style={{
+            width: "auto",
+            height: "200px",
+          }}
+          width={500}
+          height={200}
+        />
       </div>
-    </>
+      <h2 className="text-xl font-bold my-5">{name}</h2>
+      <div className="flex flexgap-4">
+        <button
+          className="py-2.5 px-5 flex items-center justify-center rounded-full text-white bg-yellow-500 hover:bg-yellow-600 transition-all duration-500 cursor-pointer"
+          onClick={() => setShowModal(true)}
+        >
+          Reservar
+        </button>
+      </div>
+    </div>
   );
 };
 
