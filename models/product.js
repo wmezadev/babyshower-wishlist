371eb0f552cb6b15ef84c8f8ee8db2b732445d3c
wmezadev <5531063+wmezadev@users.noreply.github.com>
2024-08-07 18:02:33 -0500

    feat: remove mysql and setup mongodb

---
+++ models/product.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  is_locked: {
+    type: Boolean,
+    required: true,
+  },
+  locked_by: {
+    type: String,
+    required: false,
+  },
+  last_updated: {
+    type: String,
+    required: false,
+  },
+});
+
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
