8ff7d88a8926501c2527f5dca764c2f80020d50b
wmezadev <5531063+wmezadev@users.noreply.github.com>
2024-08-15 22:01:00 -0500

    feat: avoid caching in api side

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -5,9 +5,19 @@ import Product from "../../../models/product";
 export async function GET(request) {
   try {
     await connectToDatabase();
-    const product = await Product.find({});
-    return NextResponse.json({ products: product });
+    const products = await Product.find({});
+    // Create and return the response with headers to prevent caching
+    const response = NextResponse.json({ products });
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Expires', '0');
+    response.headers.set('Pragma', 'no-cache');
+    return response;
   } catch (error) {
-    return NextResponse.json({ error: error, products: [] }, { status: 500 });
+    // Create and return the error response with headers to prevent caching
+    const errorResponse = NextResponse.json({ error: error.message, products: [] }, { status: 500 });
+    errorResponse.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    errorResponse.headers.set('Expires', '0');
+    errorResponse.headers.set('Pragma', 'no-cache');
+    return errorResponse;
   }
 }
